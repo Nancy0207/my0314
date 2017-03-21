4e4e205a70274cb1f621001c1eafc1e2cb4d864d
錢盈璇 <b6277k@gmail.com>
2017-03-21 19:55:14 +0800

    Signed-off-by: 錢盈璇 <b6277k@gmail.com>

--- ex-計程車車資計算/計程車車資計算/Form1.vb
+++ ex-計程車車資計算/計程車車資計算/Form1.vb
@@ -7,4 +7,8 @@
         intM = (Int((dblX - 1.25) / 0.25)) * 5 + 70
         TextBox2.Text = intM
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
